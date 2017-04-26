7b5bdfec6679e862bd1bdcbf025e54681a9a5094
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-04-26 12:30:55 -0500

    Took out extra stuff from my french page

--- js/french.js
+++ js/french.js
@@ -1,9 +1,7 @@
 console.log("Salut!");
 
 var Lexicon = (function(){
-	var string = "Happy Mother's Day! To the best mother in the whole world!"
 	var french = {"happy": "bonne", "mother's": "mère", "day!": "journée", "to": "á", "the": "la", "best": "meillure", "mother": "mère", "in": "dans", "whole": "entier", "world!": "monde"};
-	var selectLanguage = document.getElementById("selectLanguage");
 	return {
 		getFrench: function(){
 			console.log("Hi, this is the french page");
